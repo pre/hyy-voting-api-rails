9ec5f64ec347fdbc951995de111be877f9fe31d1
Petrus Repo <petrus.repo@iki.fi>
2016-02-25 12:15:40 +0200

    doc (setup) Improve dev env bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# TODO: Move these under correct namespace in lib/tasks/seed
 
 Voter.create!(
   :name => "Testi Pekkanen",
@@ -45,7 +38,7 @@ Election.create!(
 )
 
 Alliance.create!(
-  name: "Ruotsalaiset",
+  name: "Kauppiaat",
   election: Election.first,
   faculty: Faculty.first,
   numbering_order: 1
@@ -58,8 +51,70 @@ Candidate.create!(
   spare_firstname: "Vahva",
   spare_lastname: "Kangas",
   ssn: "123456789A",
-  candidate_number: 1,
-  numbering_order: 1
+  candidate_number: 2,
+  numbering_order: 2
+)
+
+Candidate.create!(
+  alliance: Alliance.first,
+  firstname: "S-Market",
+  lastname: "Etukortti",
+  spare_firstname: "Vihreä",
+  spare_lastname: "Muovi",
+  ssn: "123456710A",
+  candidate_number: 3,
+  numbering_order: 3
+)
+
+Candidate.create!(
+  alliance: Alliance.first,
+  firstname: "Kesko",
+  lastname: "Plussa",
+  spare_firstname: "Etu",
+  spare_lastname: "Seteli",
+  ssn: "123456711A",
+  candidate_number: 4,
+  numbering_order: 4
+)
+
+Alliance.create!(
+  name: "Asiakkaat",
+  election: Election.first,
+  faculty: Faculty.first,
+  numbering_order: 2
+)
+
+Candidate.create!(
+  alliance: Alliance.last,
+  firstname: "Seppo",
+  lastname: "Suomalainen",
+  spare_firstname: "Sami",
+  spare_lastname: "Salaperäinen",
+  ssn: "223456789A",
+  candidate_number: 5,
+  numbering_order: 5
+)
+
+Candidate.create!(
+  alliance: Alliance.last,
+  firstname: "Marja",
+  lastname: "Puuro",
+  spare_firstname: "Puolukka",
+  spare_lastname: "Rahka",
+  ssn: "323456789A",
+  candidate_number: 6,
+  numbering_order: 6
+)
+
+Candidate.create!(
+  alliance: Alliance.last,
+  firstname: "Smultron",
+  lastname: "Robotten",
+  spare_firstname: "Great",
+  spare_lastname: "Balance",
+  ssn: "423456789A",
+  candidate_number: 7,
+  numbering_order: 7
 )
 
 
